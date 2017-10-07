3698735edf28501e354eb117d11391465fd4c2a9
nobuti <buti@nobuti.com>
2017-10-07 08:43:11 +0200

    Colors.

---
+++ utils/colors.js
@@ -0,0 +1,7 @@
+export const black = '#282A36';
+export const red = '#FD605D';
+export const green = '#5FF691';
+export const yellow = '#F3F9A1';
+export const blue = '#5BC6FD';
+export const pink = '#FE6DC1';
+export const white = '#FFF';
